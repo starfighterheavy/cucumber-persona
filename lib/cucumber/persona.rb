18603ae650a7937c547a3aee6b277966495135ba
Jonathan Kirst <jskirst@gmail.com>
2017-10-02 10:56:46 -0700

    Basic Persona module and base class

--- lib/cucumber/persona.rb
+++ lib/cucumber/persona.rb
@@ -2,6 +2,29 @@ require "cucumber/persona/version"
 
 module Cucumber
   module Persona
-    # Your code goes here...
+    def self.find(name)
+      @personas ||= []
+      @personas[name] || (raise "No persona found for #{name}")
+    end
+
+    def self.define(name, &block)
+      @personas ||= {}
+      @personas[name] = BasePersona.new(name, &block)
+    end
+
+    def self.create_all
+      @personas.each { |name, persona| persona.create }
+    end
+
+    class BasePersona
+      def initialize(name, &block)
+        @name = name
+        @create_block = block
+      end
+
+      def create
+        @create_block.call
+      end
+    end
   end
 end
